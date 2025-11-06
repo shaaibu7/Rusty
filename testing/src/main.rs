aa31debe9471d0b0d6d20db5a4e2bb47edc5ae57
shaaibu7 <shaaibusuleiman9@gmail.com>
2025-11-06 07:19:45 +0100

    chore: update tests

--- testing/src/main.rs
+++ testing/src/main.rs
@@ -1,3 +1,23 @@
+enum Gender {
+    Male,
+    Female
+}
+
+
+struct Student {
+    name: String,
+    age: u8,
+    gender: Gender,
+    height: f64
+}
+
+impl Student {
+    fn get_student_name(&self) -> &String {
+        let name = &self.name;
+        name
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
@@ -25,5 +45,13 @@ mod tests {
         let mut numbers = vec![1, 2, 3, 4];
         append_data(&mut numbers, 5);
         assert_eq!(numbers.len(), 5)
+    }
+
+    #[test]
+    fn test_get_student() {
+        let student = Student{name: String::from("suleiman"), age: 17, gender: Gender::Male, height: 98.1};
+        let student_name = student.get_student_name();
+
+        assert_eq!(student_name, "suleiman")
     }
 }
