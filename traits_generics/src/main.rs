5f14d1eda3a1433158616cf7aa6bdc6eed45bd3b
shaaibu7 <shaaibusuleiman9@gmail.com>
2025-11-10 07:38:31 +0100

    chore: implement basic generics and traits

--- traits_generics/src/main.rs
+++ traits_generics/src/main.rs
@@ -1,3 +1,32 @@
+use std::ops::Mul;
+
+struct Square<T> {
+    side: u64,
+    dimension: T
+}
+
+trait ShapeTraits {
+    fn area(&self) -> u64;
+    fn perimeter(&self) -> u64;
+}
+
+impl<T> ShapeTraits for Square<T> where T: Copy + Clone + Mul {
+    fn area(&self) -> u64 {
+        self.side * self.side
+    }
+
+    fn perimeter(&self) -> u64 {
+        self.side * 4
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let square = Square{side: 5, dimension: 2};
+
+    let square_area = square.area();
+    let square_perimeter = square.perimeter();
+
+    let _ = square.dimension;
+
+    println!("The area of the rectangle is {} and the perimeter for the rectangle is {}", square_area, square_perimeter);
 }
